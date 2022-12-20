f78d06e20367eedc0627377b0f11e2b826a74b57
alex-willi <alex.lawson.williams@gmail.com>
2022-12-20 15:39:09 -0600

    make and render header nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import TodosContainer from './containers/TodosContainer';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="container">
+      <Header/>
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route path='/todos' element={<TodosContainer/>}/>
