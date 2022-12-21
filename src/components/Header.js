0cb8a5466a1ce586f4f5ffa5b2278ba12e8509b1
alex-willi <alex.lawson.williams@gmail.com>
2022-12-20 18:54:49 -0600

    add provided css

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header=()=>{
     return(
         <header>
-            <h1>ToDo</h1>
+            <h1>To do</h1>
             <nav>
                 <Link to={'/'}>HOME&emsp;</Link>
                 <Link to={'/todos'}>TODOS</Link>
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -3,9 +3,9 @@ import React, {useState} from 'react';
 const TodosContainer = () => {
 
     return (
-      <h2>
-        I am the TodosContainer page
-      </h2>
+    <div className='todosContainer' >
+      <h2>I am the TodosContainer page</h2>
+    </div>
     );
 
 };
